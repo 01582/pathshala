bd6b16b38ad53af3ec14df74953ed05fc9ce3627
Worker <150913511+xxx983@users.noreply.github.com>
2023-11-26 13:35:58 +0000

    up

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/server"
+import { db } from "@/lib/db";
+import { useState } from "react";
 
 
 const {
@@ -10,11 +12,27 @@ const {
 
 
 export const GET = async (req:Request, res: Response) => {
+    const [tokenValid, setTokenValid] = useState('');
+
     console.log(await isAuthenticated())
     const Auth = await isAuthenticated()
     const Token = await getAccessToken()
-    
-    return NextResponse.json({auth: ` ${Auth} `, token: `${Token}` })
+    try {
+        const user = await db.user.findUnique({
+            where: { tokens: { has: Token } },
+          });
+          
+    if (user) {
+       console.log("Token valid")
+       setTokenValid('true')
+      } else {
+        console.log('Invalid token' );
+        setTokenValid('false')
+      }
+    } catch (error) {
+        
+    }
+    return NextResponse.json({auth: ` ${Auth} `, token: `${Token}`, isValid: `${tokenValid}` })
 }
 export const POST = async (req:Request, res: Response) => {
     console.log("No post request allowed 402")
