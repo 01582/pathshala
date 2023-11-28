077a06b39def1be982683d899a8f88b10046ec1e
Worker <150913511+xxx983@users.noreply.github.com>
2023-11-28 13:59:02 +0000

    done auth

--- app/api/regdit/route.ts
+++ app/api/regdit/route.ts
@@ -1,6 +1,8 @@
 
 import { NextResponse } from "next/server"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/server"
+import checkTokenInDb from "@/components/p/CheckToken";
+
 
 
 
@@ -19,8 +21,10 @@ export const GET = async (req:Request, res: Response) => {
     const Auth = await isAuthenticated()
     const Token = await getAccessToken()
     console.log(Token)
+    const user = await checkTokenInDb("hello")
+    console.log(user)
    
-    return NextResponse.json({auth: ` ${Auth} `, token: `${Token}`,  })
+    return NextResponse.json({auth: ` ${Auth} `, token: `${Token}`, login: `${user}`    })
 }
 export const POST = async (req:Request, res: Response) => {
     console.log("No post request allowed 402")
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,30 @@
+'use client'
 import React from "react";
 import { toast, Toaster } from "sonner";
-
-
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 export default function Dashboard() {
+
+    const router = useRouter();
+
+	useEffect(() => {
+		axios.get('/api/regdit').then(async response => {
+			console.log(response.data.login);
+		
+		    if(response.data.login =="false"){
+				router.push("/register")
+				toast.success("Fill up the form and continue 🎉")
+			}
+	
+			
+		});
+		
+		
+
+
+		
+	  }, []); 
 
 
 	return (
